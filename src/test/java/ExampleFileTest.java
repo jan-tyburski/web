843c7b64c5dab67ead0a3164687ef8236bf3632e
Jan <jantyburski@gmail.com>
2020-05-31 05:33:29 +0200

    Example test: enter on google page and search "Polska" text and find element then is url to Wikipedia page.

--- src/test/java/ExampleFileTest.java
+++ src/test/java/ExampleFileTest.java
@@ -1,6 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class ExampleFileTest {
 
@@ -13,17 +23,21 @@ public class ExampleFileTest {
     }
 
     @Test
-    public void enterPage() {
+    public void enterPage() throws InterruptedException {
         System.out.println("Test page");
-//        ChromeOptions chromeOptions = new ChromeOptions();
-//        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-//        WebDriver driver = new ChromeDriver(chromeOptions);
-//        try {
-//            // Navigate to Url
-//            driver.get("https://google.com");
-//        } finally {
-//            driver.quit();
-//        }
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setBinary(firefoxBinary);
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("Polska\n"); // send also a "\n"
+        element.submit();
+
+        WebElement myDynamicElement = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[6]/div[2]/div[9]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/a/h3")));
+
     }
 
     @Test
